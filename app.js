f9d44e57402a6f12a3b4101ddb4a614777770783
simonet85 <christian.85@live.fr>
2020-07-31 15:20:10 +0200

    add, remove, mark all as done feature added

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const app = new Vue({
-    el: '#app',
-    data() {
-        return {
-            title: 'Hello Coding Garden'
-        }
+  el: "#app",
+  data() {
+    return {
+      title: "Hello Coding Garden",
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        done: false,
+      });
+      this.newTodo = "";
     },
-    methods: {
-        addTodo() {
-            console.log("form submitted");
-        }
+    removeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      //splice():remove elements from array , which element(todoIndex) to be rmoved and how many (1)
+      this.todos.splice(todoIndex, 1);
     },
-})
+    markAll() {
+      this.todos.forEach((todo) => {
+        todo.done = true;
+      });
+    },
+  },
+});
